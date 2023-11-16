c74e316cfe9b2c49ef99fc4053eac56942e8ee6d
kdj3331 <150220406+drrrrKim@users.noreply.github.com>
2023-11-16 14:07:42 +0900

    feature random choice item
    - royal items
    - wonki berry items
    - random choice
    #1

---
+++ crawling/wonki_berry.py
@@ -0,0 +1,56 @@
+import requests
+import csv
+import os
+
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+def crawling_wonki_berry():
+    load_dotenv()
+    url = os.environ.get("WONKI_BERRY_URL")
+
+    res = requests.get(url)
+    content = res.text
+    soup = BeautifulSoup(content, 'html.parser')
+    table = soup.select_one('#container > div > div.contents_wrap > table')
+
+    dataset = []
+
+    if table:
+        rows = table.find_all('tr')
+        
+        # hedader logic
+        header_row = rows[0]
+        headers = [header.get_text(strip=True) for header in header_row.find_all('th')[1:]]
+        headers = list(reversed(headers))
+
+        for row in rows[1:]:
+            columns = [col.get_text(strip=True) for col in reversed(row.find_all('td'))]
+            dataset.append(dict(zip(headers, columns)))
+        
+        if csv_wonki_berry(headers,dataset):
+            print('create csv sucess')
+            return True
+        else:
+            print('create csv fail')
+            return False
+    else:
+        print('table no data')
+        return False
+
+def csv_wonki_berry(headers, dataset):
+    try:
+        csv_file_path = '../data/wonkiberry.csv'
+        with open(csv_file_path, 'w', newline='', encoding='utf-8') as csvfile:
+
+            writer = csv.DictWriter(csvfile, fieldnames=headers)
+            writer.writeheader()
+            
+            for data in dataset:
+                writer.writerow(data)
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+crawling_wonki_berry()
