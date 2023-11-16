83536d5c265cd3839f74cf6334d7050a8f48e27f
drrrrKim <rlaehdwn3331@gmail.com>
2023-11-16 17:04:03 +0900

    Feat 재획 체크리스트
    - 재획 도핑 리스트 추가

--- app.py
+++ app.py
@@ -12,6 +12,23 @@ path_dir = os.path.dirname(os.path.realpath(__file__))
 token = os.environ.get("DISSCODE_TOKEN")
 prefix = '/'
 
+# discord custom emoji id
+extreme_gold = os.environ.get("EXTREME_GOLD")
+gc = os.environ.get("GC")
+jh = os.environ.get("GH")
+mvp = os.environ.get("MVP")
+exp = os.environ.get("EXP")
+vip_doping = os.environ.get("VIP_DOPING")
+vip_exp = os.environ.get("VIP_EXP")
+
+cernium = os.environ.get("CERNIUM")
+hotel = os.environ.get("HOTEL")
+odium = os.environ.get("ODIUM")
+shangrila = os.environ.get("SHANGRILA")
+arteria = os.environ.get("ARTERIA")
+carcion = os.environ.get("CARCION")
+urus = os.environ.get("URUS")
+
 # discord intents setting
 intents = discord.Intents.default()
 intents.typing = False
@@ -58,5 +75,10 @@ async def 로얄(interaction:discord.Interaction, 갯수: int):
 
         await interaction.response.send_message(embed=embed)
 
+@bot.command()
+async def 좩(ctx):
+    checklist_message = await ctx.send(f"재획 List:\n\n{jh} 재획비\n{gc} 경축비\n{mvp} 경뿌 \n{exp} 2배 \n{extreme_gold} 익골\n{vip_exp} vip")
+    for emoji in [extreme_gold,gc,jh,mvp,exp,vip_exp ]:
+        await checklist_message.add_reaction(emoji)
 
 bot.run(token)
