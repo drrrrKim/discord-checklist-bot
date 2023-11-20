b17603077ae75a67749d0fb6dbe6f5c20bd4b9e4
kdj3331 <150220406+drrrrKim@users.noreply.github.com>
2023-11-20 17:21:29 +0900

    Feat : event output func
    - csv.read -> return event
    #7

--- service/maple_event.py
+++ service/maple_event.py
@@ -1,7 +1,10 @@
 import csv
-import random
 
-from crawling import event
-async def maple_event():
-    return event.crawling_event()
-
+async def maple_event(path_dir):
+    with open(f'{path_dir}/data/event.csv', newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        items_weights = []
+        next(reader)
+        for data in reader:
+            items_weights.append(data)
+        return items_weights
