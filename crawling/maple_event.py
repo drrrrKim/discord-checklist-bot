2766b807d3989cf9f5d6b52e5d51332c37a0ca82
kdj3331 <150220406+drrrrKim@users.noreply.github.com>
2023-11-20 17:02:49 +0900

    Feat : event data
    - Add event data func crawling
    - Add event data

---
+++ crawling/maple_event.py
@@ -0,0 +1,53 @@
+import requests
+import csv
+import os
+
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+def event_co():
+    load_dotenv()
+    url = os.environ.get("EVENT_URL")
+    maple_url = os.environ.get("MAPLESTORY_URL")
+
+    res = requests.get(url)
+    content = res.text
+    soup = BeautifulSoup(content, 'html.parser')
+    ul = soup.select_one('#container > div > div.contents_wrap > div.event_board > ul')
+    dataset=[]
+    headers=["이벤트명","기간","링크"]
+
+    if ul:
+        rows = ul.select('li')
+        for li in rows:
+            data_tag = li.select_one('div.event_list_wrap dl dd.data')
+            if data_tag:
+                link = data_tag.select_one('p a')['href']
+                title = data_tag.select_one('p a').get_text(strip=True)
+                date = li.select_one('div.event_list_wrap dl dd.date p').get_text(strip=True)
+
+                columns = [title, date,f'{maple_url}{link}']
+                dataset.append(dict(zip(headers, columns)))
+
+        if csv_event(headers,dataset):
+            print('create csv sucess')
+            return True
+        else:
+            print('create csv fail')
+            return False
+    else:
+        print('ul no data')
+
+def csv_event(headers, dataset):
+    try:
+        csv_file_path = '../data/event.csv'
+        with open(csv_file_path, 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=headers)
+            writer.writeheader()
+            
+            for data in dataset:
+                writer.writerow(data)
+        return True
+    except Exception as e:
+        print(e)
+        return False
