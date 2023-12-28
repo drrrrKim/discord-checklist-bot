1529f7a2c0d1e3b911e607872fd72c9b31c8161c
drrrrKim <rlaehdwn3331@gmail.com>
2023-12-28 14:05:33 +0900

    refactor : naming, code refactoring

--- crawling/royal.py
+++ crawling/royal.py
@@ -1,6 +1,7 @@
 import requests
 import csv
 import os
+import logging
 
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
@@ -9,46 +10,51 @@ async def crawling_royal(path_dir):
     load_dotenv()
     url = os.environ.get("ROYAL_URL")
 
-    res = requests.get(url)
-    content = res.text
-    soup = BeautifulSoup(content, 'html.parser')
-    table = soup.select_one('#container > div > div.contents_wrap > table')
-
-    dataset = []
-
-    if table:
-        rows = table.find_all('tr')
-        
-        # hedader logic
-        header_row = rows[0]
-        headers = [header.get_text(strip=True) for header in header_row.find_all('th')[1:]]
-        headers = list(reversed(headers))
-
-        for row in rows[1:]:
-            columns = [col.get_text(strip=True) for col in reversed(row.find_all('td'))]
-            dataset.append(dict(zip(headers, columns)))
-        
-        if await csv_royal(headers,dataset,path_dir):
-            print('create csv sucess')
-            return True
-        else:
-            print('create csv fail')
-            return False
-    else:
-        print('table no data')
-        return False
-
-async def csv_royal(headers, dataset,path_dir):
     try:
-        csv_file_path = f'{path_dir}/data/royal.csv'
-        with open(csv_file_path, 'w', newline='', encoding='utf-8') as csvfile:
+        res = requests.get(url)
+        res.raise_for_status()
 
+        content = res.text
+        soup = BeautifulSoup(content, 'html.parser')
+        table = soup.select_one('#container > div > div.contents_wrap > table')
+
+        dataset = []
+
+        if table:
+            rows = table.find_all('tr')
+        
+            header_row = rows[0]
+            headers = [header.get_text(strip=True) for header in header_row.find_all('th')[1:]]
+            headers = list(reversed(headers))
+
+            for row in rows[1:]:
+                columns = [col.get_text(strip=True) for col in reversed(row.find_all('td'))]
+                dataset.append(dict(zip(headers, columns)))
+        
+            if await write_csv(headers, dataset, path_dir):
+                print('csv file create success')
+                return True
+            else:
+                print('csv file create fail')
+                return False
+        else:
+            print('royal list empty!')
+            return False
+        
+    except requests.RequestException as e:
+        logging.error(f'http request error : {e}')
+        return False
+    
+async def write_csv(headers, dataset, path_dir):
+    try:
+        csv_file_path = os.path.join(path_dir, 'data', 'royal.csv')
+        with open(csv_file_path, 'w', newline='', encoding='utf-8') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=headers)
             writer.writeheader()
-            
+
             for data in dataset:
                 writer.writerow(data)
         return True
     except Exception as e:
-        print(e)
+        logging.error(f'csv writer error : {e}')
         return False
--- crawling/wonki_berry.py
+++ crawling/wonki_berry.py
@@ -1,6 +1,7 @@
 import requests
 import csv
 import os
+import logging
 
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
@@ -9,46 +10,51 @@ async def crawling_wonki_berry(path_dir):
     load_dotenv()
     url = os.environ.get("WONKI_BERRY_URL")
 
-    res = requests.get(url)
-    content = res.text
-    soup = BeautifulSoup(content, 'html.parser')
-    table = soup.select_one('#container > div > div.contents_wrap > table')
-
-    dataset = []
-
-    if table:
-        rows = table.find_all('tr')
+    try:
+        res = requests.get(url)
+        res.raise_for_status()
         
-        # hedader logic
-        header_row = rows[0]
-        headers = [header.get_text(strip=True) for header in header_row.find_all('th')[1:]]
-        headers = list(reversed(headers))
+        content = res.text
+        soup = BeautifulSoup(content, 'html.parser')
+        table = soup.select_one('#container > div > div.contents_wrap > table')
 
-        for row in rows[1:]:
-            columns = [col.get_text(strip=True) for col in reversed(row.find_all('td'))]
-            dataset.append(dict(zip(headers, columns)))
+        dataset = []
+
+        if table:
+            rows = table.find_all('tr')
         
-        if await csv_wonki_berry(headers,dataset,path_dir):
-            print('create csv sucess')
-            return True
+            header_row = rows[0]
+            headers = [header.get_text(strip=True) for header in header_row.find_all('th')[1:]]
+            headers = list(reversed(headers))
+
+            for row in rows[1:]:
+                columns = [col.get_text(strip=True) for col in reversed(row.find_all('td'))]
+                dataset.append(dict(zip(headers, columns)))
+            
+            if await write_csv(headers, dataset, path_dir):
+                print('csv file create success')
+                return True
+            else:
+                print('csv file create fail')
+                return False
         else:
-            print('create csv fail')
+            print('wonki_berry list empty!')
             return False
-    else:
-        print('table no data')
+        
+    except requests.RequestException as e:
+        logging.error(f'http request error : {e}')
         return False
 
-async def csv_wonki_berry(headers, dataset, path_dir):
+async def write_csv(headers, dataset, path_dir):
     try:
-        csv_file_path = f'{path_dir}/data/wonkiberry.csv'
+        csv_file_path = os.path.join(path_dir, 'data', 'wonkiberry.csv')
         with open(csv_file_path, 'w', newline='', encoding='utf-8') as csvfile:
-
             writer = csv.DictWriter(csvfile, fieldnames=headers)
             writer.writeheader()
-            
+
             for data in dataset:
                 writer.writerow(data)
         return True
     except Exception as e:
-        print(e)
+        logging.error(f'csv writer error : {e}')
         return False
